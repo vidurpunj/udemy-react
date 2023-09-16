86b0a2d27e4d501cd40760fcb02ffa68338fbe2f
vidur <vidur.punj@hotmail.com>
2023-09-16 17:26:51 +0530

    77. Alternative: Creating A Shared Handler Function

--- src/App.js
+++ src/App.js
@@ -4,25 +4,11 @@ import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
-  // const [userInput, setUserInput] = useState({
-  //   enteredCounter: 0,
-  // })
-  // const incrementHandler = (event) => {
-  //   setUserInput((prevState) => {
-  //     return {
-  //       ...prevState,
-  //       enteredCounter: prevState.enteredCounter + 1,
-  //     }
-  //   })
-  // }
+
   return (
     <div className="App">
       <NewExpense />
       <Expenses />
-      {/* <div>
-        <p id="counter">{userInput.enteredCounter}</p>
-        <button onClick={incrementHandler}>Increment</button>
-      </div> */}
     </div>
   );
 }
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,66 +4,37 @@ import './ExpenseForm.css';
 const ExpenseForm = () => {
     const [eneterdTitle, setEneterdTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate,setEnteredDate] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
 
-    // const [userInput, setUserInput] = useState({
-    //     enteredTitle: '',
-    //     enteredAmount: '',
-    //     enteredDate: ''
-    // })
-
-    const titleChangeHandler = (event) => {
-        setEneterdTitle(event.target.value);
-        // setUserInput({
-        //     ...userInput,
-        //     enteredTitle: event.target.value,
-        // });
-        // setUserInput((prevState) => {
-        //     return {
-        //         ...prevState,
-        //         enteredTitle: event.target.value,
-        //     }
-        // })
-        console.log(event.target.value);
-    }
-    const amountChangeHanler = (event) => {
-        setEnteredAmount(event.target.value);
-        // setUserInput({
-        //     ...userInput,
-        //     enteredAmount: event.target.value,
-        // })
-        console.log(event.target.value);
-    }
-    const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
-        // setUserInput({
-        //     ...userInput,
-        //     enteredAmount: event.target.value,
-        // })
-        // setUserInput((prevState) => {
-        //     return {
-        //         ...prevState,
-        //         enteredAmount: event.target.value,
-        //     }
-        // })
-        console.log(event.target.value);
+    const eventChangeHandler = (identifier, event) => {
+        if(identifier === 'title'){
+            setEneterdTitle(event.target.value);
+            console.log('Title Changed');
+        }else if (identifier === 'amount'){
+            setEnteredAmount(event.target.value);
+            console.log('Amount Changed')
+        }
+        else if (identifier === 'date'){
+            setEnteredDate(event.target.value);
+            console.log('Date Changed')
+        }
     }
     return (
         <form>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler}></input>
+                    <input type="text" onChange={(event) => {eventChangeHandler('title', event)}}></input>
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHanler} />
+                    <input type='number' min='0.01' step='0.01' onChange={(event)=>{eventChangeHandler('amount', event)}} />
                 </div>
             </div>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type="date" min='2019-01-01' max='2023-01-01' onChange={dateChangeHandler} />
+                    <input type="date" min='2019-01-01' max='2023-01-01' onChange={(event) => {eventChangeHandler('date', event)}} />
                 </div>
             </div>
             <div className="new-expense__actions">

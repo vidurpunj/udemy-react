b12500842fba2c2ff8e8c9b9b4e2f55057a22f32
vidur <vidur.punj@hotmail.com>
2023-09-17 18:08:25 +0530

    82. Derived / Computed State

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,12 +6,25 @@ import ExpenseFilter from './ExpenseFilter';
 
 const Expenses = (props) => {
     const [filteredYear, setFilterdYear] = useState('2020');
+    
+    let filterInfoText = '2019, 2021 & 2022';
+    
+    if (filteredYear == 2019){
+        filterInfoText = '2020, 2021 & 2022';
+    }else if (filteredYear === '2020'){
+        filterInfoText = '2019, 2021 & 2022';
+    }else if(filteredYear === '2021'){
+        filterInfoText = '2019, 2020 & 2022'
+    }else if(filteredYear === '2022'){
+        filterInfoText = '2019, 2020 & 2021'
+    }
     const filterChangedHandler = (selectedYear) => {
         setFilterdYear(selectedYear);
         console.log(selectedYear);
     }
     return (
         <Card className='expenses'>
+            <h1>Data of years {filterInfoText} is not included.</h1>
             <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangedHandler}/>
             <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
             <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />

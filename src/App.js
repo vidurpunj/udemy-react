95876d7dcad10771aea493ea62d6414dc6c9e3fd
vidur <vidur.punj@hotmail.com>
2023-09-21 00:24:05 +0530

    87. Using Stateful Lists

--- src/App.js
+++ src/App.js
@@ -3,37 +3,45 @@ import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const dummy_expenses = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date(2021, 2, 12)
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 const App = () => {
+  const [expenses, setExpenses] = useState(dummy_expenses);
+
   const addExpensehandler = (enteredAddExpense) => {
+    enteredAddExpense.date = new Date(enteredAddExpense.date);
+    setExpenses((prevExpenses)=>{
+      return [enteredAddExpense, ...prevExpenses]
+    })
     console.log('inside App.js');
     console.log(enteredAddExpense);
   }
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: 'e2',
-      title: 'New TV',
-      amount: 799.49,
-      date: new Date(2021, 2, 12)
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+
   return (
     <div className="App">
       <NewExpense onAddExpense={addExpensehandler}/>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card';
@@ -6,29 +6,34 @@ import ExpenseFilter from './ExpenseFilter';
 
 const Expenses = (props) => {
     const [filteredYear, setFilterdYear] = useState('2020');
-    
+
     let filterInfoText = '2019, 2021 & 2022';
-    
-    if (filteredYear == 2019){
+
+    if (filteredYear == 2019) {
         filterInfoText = '2020, 2021 & 2022';
-    }else if (filteredYear === '2020'){
+    } else if (filteredYear === '2020') {
         filterInfoText = '2019, 2021 & 2022';
-    }else if(filteredYear === '2021'){
+    } else if (filteredYear === '2021') {
         filterInfoText = '2019, 2020 & 2022'
-    }else if(filteredYear === '2022'){
+    } else if (filteredYear === '2022') {
         filterInfoText = '2019, 2020 & 2021'
     }
     const filterChangedHandler = (selectedYear) => {
         setFilterdYear(selectedYear);
         console.log(selectedYear);
     }
+    console.log(props.items);
     return (
         <Card className='expenses'>
             <h1>Data of years {filterInfoText} is not included.</h1>
-            <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangedHandler}/>
-            <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
-            <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
-            <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date} />
+            <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangedHandler} />
+            {props.items.map((expense) => (
+                <ExpenseItem
+                    key={expense.id}
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date} />
+            ))}
         </Card>
     )
 }
